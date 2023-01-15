160281d69d92a777430b790786f6570745529fcf
altanemredemirci <altan_emre_1989@hotmail.com>
2023-01-15 20:38:27 +0300

    Index Update

--- BlogProject.UI/Controllers/HomeController.cs
+++ BlogProject.UI/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace BlogProject.UI.Controllers
         private UserManager um = new UserManager();
         // GET: Home
         public ActionResult Index()
-        {           
+        {      //Queryable      
             return View(bm.GetAllBlogQueryable().OrderByDescending(i => i.ModifiedOn).ToList());
         }
 
